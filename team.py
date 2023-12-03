ad26f1aa32e26767b79904eb675effb8bde4788f
Sam20B <wafulabibiana93@gmail.com>
2023-12-03 15:36:44 +0300

    car_dealership.py

--- team.py
+++ team.py
@@ -6,6 +6,12 @@ team = {
     'cynthia':'lisa',
     'elphas':'banard'
 }
+
+team_list = ['brian','kevin','mitchelle','esther','cynthia','elphas','']
+
+print("Choose name from the ones below: ")
+for item in team_list:
+    print(item)
 x = str(input())
 if x in team:
     print(x + " is a partner to: " + team[x])
